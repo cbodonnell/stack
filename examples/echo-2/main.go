87e4675177c4571d2b07399be05bf3e99085e521
Craig O'Donnell <cbodonnell@comcast.net>
2023-01-29 10:21:32 -0500

    fix examples

--- examples/echo-2/main.go
+++ examples/echo-2/main.go
@@ -5,17 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// add a flag to configure the port
 	var port int
-	// TODO: Fix flags
 	flag.IntVar(&port, "port", 8080, "port to listen on")
+	flag.Parse()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
 	})
+
+	// log to stdout
+	log.SetOutput(os.Stdout)
 
 	log.Printf("Listening on port %d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
